fa56f31af6beceaa0d424c552a3ea7e4b714fdf3
Egor Pavlovskiy <pixer.ru@mail.ru>
2022-06-20 12:01:59 +0300

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.send_activation_email@users = User.where(activated: FILL_IN).paginate(page: params[:page])
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Twit!"
       redirect_to @user
     else
       render 'new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@ User.create!(name: "Example User",
              password:
                "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now))
 # Generate a bunch of additional users.
 99.times do |n|
   name = Faker::Name.name
@@ -22,5 +24,7 @@ User.create!(name: "Example User",
                email: email,
                password:
                  password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now))
 end
